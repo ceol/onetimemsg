bf38294a78c0464969f5edaf15773c5506024963
Patrick Jacobs <ceolwulf@gmail.com>
2012-04-21 15:08:46 -0400

    ported __init__.py from other project

---
+++ onetimemsg/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from werkzeug import import_string, cached_property
+from werkzeug.routing import BaseConverter
+
+app = Flask(__name__)
+app.config.from_object('onetimemsg.settings')
+
+# @see: http://stackoverflow.com/a/5872904
+class RegexConverter(BaseConverter):
+    def __init__(self, url_map, *items):
+        super(RegexConverter, self).__init__(url_map)
+        self.regex = items[0]
+app.url_map.converters['regex'] = RegexConverter
+
+import onetimemsg.views
+
+if __name__ == '__main__':
+    app.run()
