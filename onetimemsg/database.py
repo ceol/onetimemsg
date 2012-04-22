9d1f6e0e1c272c1822938f5ddd34d5ae775386ce
Patrick Jacobs <ceolwulf@gmail.com>
2012-04-21 22:58:25 -0400

    created BaseQuery class for *_or_404 functionality

--- onetimemsg/database.py
+++ onetimemsg/database.py
@@ -1,31 +1,32 @@
 from flask import abort
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.orm.query import Query
 from sqlalchemy.ext.declarative import declarative_base
 from onetimemsg import app
+
+class BaseQuery(Query):
+    # @see: https://github.com/mitsuhiko/flask-sqlalchemy/blob/master/flask_sqlalchemy.py#L354
+    def get_or_404(self, ident):
+        rv = self.get(ident)
+        if rv is None:
+            abort(404)
+        return rv
+
+    # @see: https://github.com/mitsuhiko/flask-sqlalchemy/blob/master/flask_sqlalchemy.py#L363
+    def first_or_404(self):
+        rv = self.first()
+        if rv is None:
+            abort(404)
+        return rv
 
 engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'], convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
-                                         bind=engine))
+                                         bind=engine,
+                                         query_cls=BaseQuery))
 Base = declarative_base()
 Base.query = db_session.query_property()
-
-# @see: https://github.com/mitsuhiko/flask-sqlalchemy/blob/master/flask_sqlalchemy.py#L354
-def get_or_404(self, ident):
-    rv = self.get(ident)
-    if rv is None:
-        abort(404)
-    return rv
-Base.get_or_404 = get_or_404
-
-# @see: https://github.com/mitsuhiko/flask-sqlalchemy/blob/master/flask_sqlalchemy.py#L363
-def first_or_404(self):
-    rv = self.first()
-    if rv is None:
-        abort(404)
-    return rv
-Base.first_or_404 = first_or_404
 
 def init_db():
     # import all modules here that might define models so that
